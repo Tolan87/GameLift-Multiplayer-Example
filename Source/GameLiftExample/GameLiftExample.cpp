c1825c0a0987a04dc2e769d454207492a9e33fc4
DESKTOP-LS5AT12\Toni <tonilangbein@gmail.com>
2018-07-23 18:30:08 +0200

    - revert all changes

--- Source/GameLiftExample/GameLiftExample.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "GameLiftExample.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameLiftExample, "GameLiftExample" );
